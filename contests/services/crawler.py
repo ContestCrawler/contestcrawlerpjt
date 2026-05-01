f6f0c49212162ba45db313e497d4ba54a04c340c
WinterI5Coming <dutjddnr1224@naver.com>
2026-05-01 18:30:53 +0900

    feat: 공모전 리스트 페이지 html 가져오는 crawler.py 완료

--- contests/services/crawler.py
+++ contests/services/crawler.py
@@ -1,7 +1,36 @@
+from contextlib import contextmanager
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 CONTEST_URL = "https://www.data.go.kr/suc/preliminaryRound.do"
 
 
-# def fetch_contest_list_page():
+@contextmanager
+def create_driver():
+    options = Options()
+    options.add_argument("--window-size=1920, 1080")
     
+    # 나중에 추가
+    # options.add_argument("--headless=new")
+    
+    driver = webdriver.Chrome(    
+        service=Service(ChromeDriverManager().install()),
+        options=options,
+    )
+    
+    try:
+        yield driver
+    
+    finally:
+        driver.quit()
+        
+        
+def fetch_list_page():
+    with create_driver() as driver:
+        driver.get(CONTEST_URL)
+        return driver.page_source
