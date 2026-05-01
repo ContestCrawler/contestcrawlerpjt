eafb1be86e794d06c0735e9e4db57e4905d8cfb9
hw <brightrain453@gmail.com>
2026-05-01 17:32:51 +0900

    feat: main page 메인 페이지 뷰함수와 템플릿 뼈대입니다. 이후 기능이 추가되어도 추가 템플릿 없이 메인 페이지 하나로 처리할 듯 합니다.

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'serice'
+app_name = 'service'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- service/views.py
+++ service/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from contests.models import Contest
+
 
 # Create your views here.
 def index(request):
-    return
+    contests = Contest.objects.all()
+    context = {
+        'contests': contests,
+    }
+    return render(request, 'service/index.html', context)
+
+
+# 필터링 함수
+def filtering(request):
+    context = {
+    
+    }
+    return render(request, 'service/index.html', context)
+
+
+"""
+
+0. 1일 1회 데이터 수집 진행
+
+== 복잡한 로직이 없어서 관통 프로젝트 같이 인덱스 페이지 하나로 가능할 듯 ==
+
+1. 인덱스 페이지에서 전체 데이터 조회
+2. 분류, 정렬 조건 선택
+3. 데이터 분류 및 정렬 > 동적 페이지로 해야하나?
+  - 추가 페이지 구성 필요 없음
+  - 간단한 요약 및 클릭 연결
+
+"""
