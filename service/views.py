d9ca976059b46eba02cae228ee86d0a017b169c3
hw <brightrain453@gmail.com>
2026-05-03 12:11:37 +0900

    feature: create 관련 코드를 제거했습니다. 크롤링 파트에서 자동화가 완성되어 수동으로 입력하는 기능을 제거 (비활성화) 했습니다.

--- service/views.py
+++ service/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from contests.models import Contest
-from .service import MainServiceV1, CreateServiceV1
+from .service import MainServiceV1
 
 
 def index(request):
@@ -10,9 +10,9 @@ def index(request):
     return render(request, 'service/index.html', context)
 
 
-def create(request):
-    create_service = CreateServiceV1()
-    context = create_service.get_context(request)
-    if context == None:
-        return redirect('services:index')
-    return render(request, 'service/create.html', context)
+# def create(request):
+#     create_service = CreateServiceV1()
+#     context = create_service.get_context(request)
+#     if context == None:
+#         return redirect('services:index')
+#     return render(request, 'service/create.html', context)
