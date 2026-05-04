7edd8d248a097701ca5547f0e16601ca338bf1d7
lhw <brightrain453@gmail.com>
2026-05-04 17:31:39 +0900

    refactor: 스케쥴러 관련 함수를 더 내부로 이동시켰습니다.

--- service/services/schedule.py
+++ service/services/schedule.py
@@ -1,37 +1,41 @@
 # from contests.trigger import trigger
 from django.utils import timezone
-from repositories.contest_repo import ContestRepository
+from service.repositories.contest_repo import ContestRepository
 
 
-class Scheduller():
-    contest_repository = ContestRepository()
 
-    def __init__(self):
-        pass
+contest_repository = ContestRepository()
 
 
-    def start_crawl(self):
-        # return trigger()
-        # trigger() 함수가 중복 데이터 pk 값 반환
+def start_crawl():
+    # return trigger()
+    # trigger() 함수가 중복 데이터 pk 값 반환
+    pass
 
-        pass
+
+def date_update_to(limit_pk):
+    # pk 가 limit_pk 보다 작고 아직 종료되지 않은 공모전을 filtering 하는 conditions
+    conditions = {
+        'pk__lt': limit_pk,
+        'is_active': True,
+    }
+    contests = contest_repository.get_filtered(conditions)
+
+    activated_date = timezone.localdate()
+
+    for contest in contests:
+        if contest.end_date == None:
+            continue
+
+        if contest.end_date < activated_date:
+            contest.is_active = False
+            contest.deleted_at = activated_date
+            contest.save()
+        else:
+            contest.updated_at = activated_date
+    pass
 
 
-    def date_update_to(self, limit_pk):
-        # pk 가 limit_pk 보다 작고 아직 종료되지 않은 공모전을 filtering 하는 conditions
-        conditions = {
-            'pk__lt': limit_pk,
-            'is_active': True,
-        }
-        contests = self.contest_repository.get_filtered(conditions)
-
-        activated_date = timezone.localdate
-
-        for contest in contests:
-            if contest.end_date < activated_date:
-                contest.is_active = False
-                contest.deleted_at = activated_date
-                contest.save()
-            else:
-                contest.updated_at = activated_date
-        pass
+def run_scheduled_update():
+    limit_pk = start_crawl()
+    date_update_to(limit_pk)
--- service/views/scheduled_update.py
+++ service/views/scheduled_update.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from services.schedule import Scheduller
+from service.services.schedule import run_scheduled_update
+from django.http import HttpResponse
 
 
 def scheduled_update(request):
-    scheduller = Scheduller()
-    duplicated_pk = scheduller.start_crawl()
-    # duplicated_pk 값은 크롤링 진행 이전 마지막 pk 값으로 가정
-    scheduller.date_update_to(duplicated_pk)
-    return
+    run_scheduled_update()
+
+    return HttpResponse(status=204)
